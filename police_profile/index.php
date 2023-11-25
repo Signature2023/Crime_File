e804cb081b66f2e63053aabb6ddc94e532a50ef4
anjana-rajeev <anjanarajeev017@gmail.com>
2023-11-25 19:17:51 +0530

    home_dashboard_anj

--- police_profile/index.php
+++ police_profile/index.php
@@ -1,20 +1,153 @@
-<?php
-require('header.php');
-?>
+<!DOCTYPE html>
+<html>
 
-<div class="main-container">
-    <div class="pd-ltr-20 xs-pd-20-10">
-        <div class="min-height-200px">
-            <div class="page-header">
-                <div class="row">
-                   
 
-                </div>
+<head>
+
+    <?php require('header.php'); ?>
+    <!-- Site favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="vendors/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="vendors/images/favicon-16x16.png">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
+    <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="src/plugins/jvectormap/jquery-jvectormap-2.0.3.css">
+    <link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-119386393-1');
+    </script>
+</head>
+
+<body>
+
+    <div class="pre-loader">
+        <div class="pre-loader-box">
+            <div class="loader-logo"><img src="vendors/images/deskapp-logo.svg" alt=""></div>
+            <div class='loader-progress' id="progress_div">
+                <div class='bar' id='bar1'></div>
             </div>
-            <div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
+            <div class='percent' id='percent1'>0%</div>
+            <div class="loading-text">
+                Loading...
             </div>
         </div>
+    </div>
 
-        <?php
-require('footer.php');
-?>
+
+
+    <div class="main-container">
+        <div class="xs-pd-20-10 pd-ltr-20">
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="title">
+                            <h4>Home</h4>
+                        </div>
+                        <nav aria-label="breadcrumb" role="navigation">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Home</li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <div class="col-md-6 col-sm-12 text-right">
+                        <div class="dropdown">
+
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="#">Export List</a>
+                                <a class="dropdown-item" href="#">Policies</a>
+                                <a class="dropdown-item" href="#">View Assets</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row clearfix progress-box">
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                    <div class="card-box pd-30 height-100-p">
+                        <div class="progress-box text-center">
+                            <input type="text" class="knob dial1" value="80" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#1b00ff" data-angleOffset="180" readonly>
+                            <h5 class="text-blue padding-top-10 h5">Accident Report</h5>
+                            <span class="d-block">80% Average <i class="fa fa-line-chart text-blue"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                    <div class="card-box pd-30 height-100-p">
+                        <div class="progress-box text-center">
+                            <input type="text" class="knob dial2" value="70" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#00e091" data-angleOffset="180" readonly>
+                            <h5 class="text-light-green padding-top-10 h5">Lost and Found</h5>
+                            <span class="d-block">75% Average <i class="fa text-light-green fa-line-chart"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                    <div class="card-box pd-30 height-100-p">
+                        <div class="progress-box text-center">
+                            <input type="text" class="knob dial3" value="90" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#f56767" data-angleOffset="180" readonly>
+                            <h5 class="text-light-orange padding-top-10 h5">Anonymous</h5>
+                            <span class="d-block">90% Average <i class="fa text-light-orange fa-line-chart"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                    <div class="card-box pd-30 height-100-p">
+                        <div class="progress-box text-center">
+                            <input type="text" class="knob dial4" value="65" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#a683eb" data-angleOffset="180" readonly>
+                            <h5 class="text-light-purple padding-top-10 h5">Total</h5>
+                            <span class="d-block">82% Average <i class="fa text-light-purple fa-line-chart"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12 mb-30">
+                    <div class="card-box pd-30 height-100-p">
+                        <h4 class="mb-30 h4">Crime Reports Chart</h4>
+                        <!-- <div id="compliance-trend" class="compliance-trend"></div> -->
+                        <a href="vendors\images\cybercrime-statistics-reported-losses-past-decade-1024x618.png"></a>
+                        <img src="vendors/images/cybercrime-statistics-reported-losses-past-decade-1024x618.png" alt="Compliance Trend Image">
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-12 col-sm-12 mb-30">
+                    <div class="card-box pd-30 height-100-p">
+                        <h4 class="mb-30 h4">Records</h4>
+                        <div id="chart" class="chart"></div>
+                    </div>
+                </div>
+            </div>
+            <?php require('footer.php'); ?>
+        </div>
+    </div>
+    <!-- js -->
+    <script src="vendors/scripts/core.js"></script>
+    <script src="vendors/scripts/script.min.js"></script>
+    <script src="vendors/scripts/process.js"></script>
+    <script src="vendors/scripts/layout-settings.js"></script>
+    <script src="src/plugins/jQuery-Knob-master/jquery.knob.min.js"></script>
+    <script src="src/plugins/highcharts-6.0.7/code/highcharts.js"></script>
+    <script src="src/plugins/highcharts-6.0.7/code/highcharts-more.js"></script>
+    <script src="src/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js"></script>
+    <script src="src/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="vendors/scripts/dashboard2.js"></script>
+</body>
+
+</html>
